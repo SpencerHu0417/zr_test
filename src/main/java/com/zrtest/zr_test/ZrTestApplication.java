75b7f8d843d5ac1147aa0b581f779de8dee5eea9
Spencer <huchentj@163.com>
2019-08-06 23:45:57 +0800

    项目框架 第一次提交

---
+++ src/main/java/com/zrtest/zr_test/ZrTestApplication.java
@@ -0,0 +1,13 @@
+package com.zrtest.zr_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZrTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZrTestApplication.class, args);
+    }
+
+}
